680cb8fa46d453baf2e3c0b24deb983b4d36f677
HieuTran <graynatsu80@gmail.com>
2023-12-05 23:47:58 +0700

    SUCCESSFULL DONE , I'M FREE

--- test/test.js
+++ test/test.js
@@ -1,52 +1,59 @@
 
-const listImages = document.querySelector('.list-images')
+const listProduction = document.querySelectorAll('.list-production')
+let current = 0
 
-for(let item of listImages) {
+for (let item of listProduction) {
+    console.log(item)
+    const partProduction = item.querySelectorAll('.part-production')
+    console.log(partProduction)
 
-    const imgs = item.getElementsByTagName('img')
-    let current = 0
-    const length = imgs.length
-    const btnPrev = item.getElementsByClass('.btnPrev')
-    const btnNext = item.getElementsByClass('.btnNext')
-    
-const handleChangeSilde = () => {
-    if (current == length - 1) {
-        current = 0
-        let widthImg = imgs[0].offsetWidth
-        item.style.transform = `translateX(0px)`;
-        item.querySelector('.active').classList.remove('active')
+    const length = partProduction.length
+    const btnPrev = item.querySelector('.btnPrev')
+    const btnNext = item.querySelector('.btnNext')
+    console.log(btnNext)
+    console.log(btnPrev)
+
+    const handleChangeSilde = () => {
+        if (current == length - 1) {
+            current = 0
+            let widthImg = partProduction[0].offsetWidth
+            item.style.transform = `translateX(0px)`;
+            item.querySelector('.active').classList.remove('active')
+        }
+        else {
+            current++
+            let widthImg = partProduction[0].offsetWidth;
+            item.style.transform = `translateX(${widthImg * -1 * current}px)`;
+        }
     }
-    else {
-        current++
-        let widthImg = imgs[0].offsetWidth;
-        item.style.transform = `translateX(${widthImg * -1 * current}px)`;
-    }
+
+    let handleEventChangeSilde = setInterval(handleChangeSilde, 4000)
+
+
+    btnNext.addEventListener('click', () => {
+        clearInterval(handleEventChangeSilde)
+        handleChangeSilde()
+        handleEventChangeSilde = setInterval(handleChangeSilde, 4000)
+
+    })
+    btnPrev.addEventListener('click', () => {
+        clearInterval(handleEventChangeSilde)
+        if (current == 0) {
+            current = length - 1
+            let widthImg = partProduction[0].offsetWidth;
+            listImages.style.transform = `translateX(${widthImg * -1 * current}px)`;
+        }
+        else {
+            current--
+            let widthImg = partProduction[0].offsetWidth;
+            listImages.style.transform = `translateX(${widthImg * -1 * current}px)`;
+        }
+        handleEventChangeSilde = setInterval(handleChangeSilde, 4000)
+
+
+    })
 }
 
-let handleEventChangeSilde = setInterval(handleChangeSilde, 4000)
-btnNext.addEventListener('click', () => {
-    clearInterval(handleEventChangeSilde)
-    handleChangeSilde()
-    handleEventChangeSilde = setInterval(handleChangeSilde, 4000)
-
-})
-btnPrev.addEventListener('click', () => {
-    clearInterval(handleEventChangeSilde)
-    if (current == 0) {
-        current = length - 1
-        let widthImg = imgs[0].offsetWidth;
-        item.style.transform = `translateX(${widthImg * -1 * current}px)`;
-    }
-    else {
-        current--
-        let widthImg = imgs[0].offsetWidth;
-        item.style.transform = `translateX(${widthImg * -1 * current}px)`;
-    }
-    handleEventChangeSilde = setInterval(handleChangeSilde, 4000)
-
-})
-    
-}
 
 // const listImages = document.querySelector('.list-images')
 // const imgs = listImages.getElementsByTagName('img')
@@ -72,6 +79,7 @@ btnPrev.addEventListener('click', () => {
 // }
 
 // let handleEventChangeSilde = setInterval(handleChangeSilde, 4000)
+
 // btnNext.addEventListener('click', () => {
 //     clearInterval(handleEventChangeSilde)
 //     handleChangeSilde()
